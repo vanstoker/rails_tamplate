f587e7ed2c5029d95e7a7f6db620d03a0d8b20b6
vanstoker <vanstoker@mail.ru>
2023-06-05 23:24:53 +0300

    chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  before_save { self.email = email.downcase }
+  # before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name,     presence: true,
                        length: { maximum: 50 }
