3bbc6b8bd8d9f1cec566c2f1bd0032b70668e04d
vanstoker <vanstoker@mail.ru>
2023-06-20 23:50:45 +0300

    temp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  # before_save { self.email = email.downcase }
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase }
 
   validates :name,     presence: true,
                        length: { maximum: 50 }
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      user_params = {
+         name:  '',
+         email: 'user@invalid',
+         password: 'foo',
+         password_confirmation: 'bar'
+      }
+
+      post users_path, params: { user: user_params }
+    end
+    assert_template 'users/new'
+  end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      user_params = {
+        name: 'Example User',
+        email: 'user@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+
+      post users_path, params: { user: user_params }
+      follow_redirect!
+    end
+    assert_template 'users/show'
+  end
+end
